713f5a67e22995574ab0c3a7adbd9a73959ebfa6
Huzaifa <meharhuzaifa540@gmail.com>
2024-04-24 04:49:18 -0700

    created-projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>React in basics</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>React with Vite Basics in Learning</h1>
+  )
+}
+
+export default App
