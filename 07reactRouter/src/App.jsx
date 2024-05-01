577e6bc07e213297ca11ab6fde6e9131403d372c
Huzaifa <meharhuzaifa540@gmail.com>
2024-05-01 04:06:16 -0700

    miniuseContext

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header/Header'
-import Home from './components/Home/Home'
-import Footer from './components/Footer/Footer'
+
 
 function App() {
   const [count, setCount] = useState(0)
